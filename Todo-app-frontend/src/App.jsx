db14f98ea8a096665453c426b4d0d3972eff9389
ayanchavand <ayan.chavand15@gmail.com>
2024-08-13 19:14:09 +0530

    Front End initialization

--- Todo-app-frontend/src/App.jsx
+++ Todo-app-frontend/src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return
+  return (
+    <>
+      <h1>TEST FRONTEND LMFAO</h1>
+      <h6> also like idfk what I am getting into so if you are reading this please send help, I need therapy</h6>
+    </>
+  )
 }
+
 
 export default App
